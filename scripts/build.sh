04bc6e4751269b27e737ddc8f84c67fd697c0413
ton9q <kuchma_anton@mail.ru>
2019-02-24 16:08:23 +0300

    chore(env, init files, data, heroku): add package, webpack, eslint with dependencies

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+echo "---Building CLIENT part---"
+npm run client-build
+echo "---CLIENT part was built successfuly---"
+
+echo "---Building SERVER part---"
+npm run server-build
+echo "---SERVER part was built successfuly---"
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Mentor Dashboard',
+      template: 'public/index.html',
+    }),
+  ],
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 1902,
+  },
+};
